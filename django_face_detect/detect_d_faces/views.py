7e124d3357824e18de2f474d60aeba8524c65839
stagwithadream <sairamreddy4299@gmail.com>
2019-03-24 07:36:53 +0530

    pushed the output to browser

--- django_face_detect/detect_d_faces/views.py
+++ django_face_detect/detect_d_faces/views.py
@@ -4,12 +4,14 @@ from django.core.files.storage import FileSystemStorage
 import numpy as np
 import argparse
 import cv2
+from django.conf import settings
+import os
 
 filename= None
 
 
+STATIC_DIR=settings.STATIC_DIR
 
-# Create your views here.
 def index(request):
 
 
@@ -17,45 +19,34 @@ def index(request):
 
         myfile=request.FILES['myfile']
         fs=FileSystemStorage()
+
+
         filename=fs.save(myfile.name,myfile)
+        os.rename(fs.url(filename),os.path.join(STATIC_DIR,'detect_d_faces/images/input/'+fs.url(filename)))
+        print(os.path.join(STATIC_DIR,'detect_d_faces/images/input/'+fs.url(filename)))
 
 
-        # construct the argument parse and parse the arguments
+        net = cv2.dnn.readNetFromCaffe(os.path.join(STATIC_DIR,'detect_d_faces/pyEssentials/deploy.prototxt.txt'),os.path.join(STATIC_DIR,'detect_d_faces/pyEssentials/res10_300x300_ssd_iter_140000.caffemodel') )
 
-
-        # load our serialized model from disk
-
-        net = cv2.dnn.readNetFromCaffe('/home/sairam/Desktop/face_detect/django_face_detect/detect_d_faces/deploy.prototxt.txt','/home/sairam/Desktop/face_detect/django_face_detect/detect_d_faces/res10_300x300_ssd_iter_140000.caffemodel' )
-
-        # load the input image and construct an input blob for the image
-        # by resizing to a fixed 300x300 pixels and then normalizing it
-        image = cv2.imread('/home/sairam/Desktop/face_detect/django_face_detect/'+fs.url(filename))
+        image = cv2.imread(os.path.join(STATIC_DIR,'detect_d_faces/images/input/'+fs.url(filename)))
         (h, w) = image.shape[:2]
         blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
         	(300, 300), (104.0, 177.0, 123.0))
 
-        # pass the blob through the network and obtain the detections and
-        # predictions
-
         net.setInput(blob)
         detections = net.forward()
 
-        # loop over the detections
+
         for i in range(0, detections.shape[2]):
-        	# extract the confidence (i.e., probability) associated with the
-        	# prediction
+
         	confidence = detections[0, 0, i, 2]
 
-        	# filter out weak detections by ensuring the `confidence` is
-        	# greater than the minimum confidence
+
         	if confidence > 0.5 :
-        		# compute the (x, y)-coordinates of the bounding box for the
-        		# object
+
         		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         		(startX, startY, endX, endY) = box.astype("int")
 
-        		# draw the bounding box of the face along with the associated
-        		# probability
         		text = "{:.2f}%".format(confidence * 100)
         		y = startY - 10 if startY - 10 > 10 else startY + 10
         		cv2.rectangle(image, (startX, startY), (endX, endY),
@@ -63,11 +54,15 @@ def index(request):
         		cv2.putText(image, text, (startX, y),
         			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-        # show the output image
-        cv2.imshow("Output", image)
-        cv2.imwrite("output"+fs.url(filename),image)
-        fs.delete(filename)
-        cv2.waitKey(0)
+
+
+        cv2.imwrite(os.path.join(STATIC_DIR,'detect_d_faces/images/output/output.jpg'),image)
+        return output_display(request)
+
 
 
     return render(request,'index.html')
+
+
+def output_display(request):
+    return render(request,'displayOutput.html')
